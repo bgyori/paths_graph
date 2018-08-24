dec7a44e0b29d5d53ba1ab5d82a6707f27ef8bc9
Ben Gyori <ben.gyori@gmail.com>
2018-08-24 18:46:28 -0400

    Refactor path checker tests

--- paths_graph/tests/test_path_checker.py
+++ paths_graph/tests/test_path_checker.py
@@ -1,37 +1,43 @@
 from paths_graph.path_checker import PathChecker
 
 
+def _run_cases(test_cases):
+    for formula, nodes, tf_expected in test_cases:
+        pc = PathChecker(formula, nodes)
+        assert pc.truth == tf_expected, \
+            'Got unexpected %s for %s with nodes %s' % (pc.truth, formula,
+                                                        str(nodes))
+
+
 def test_path_checker_batch_atomic():
-    formula = '[5]'
-    pc = PathChecker(formula, ['5'])
-    assert pc.truth == True
-    pc = PathChecker(formula, ['4'])
-    assert pc.truth == False
-    pc = PathChecker(formula, ['5', '4'])
-    assert pc.truth == True
-    pc = PathChecker(formula, ['4', '5'])
-    assert pc.truth == False
+    test_cases = [('[5]', ['5'], True),
+                  ('[5]', ['4'], False),
+                  ('[5]', ['5', '4'], True),
+                  ('[5]', ['4', '5'], False)]
+    _run_cases(test_cases)
+
+
+def test_path_checker_batch_atomic_neg():
+    test_cases = [('![5]', ['5'], False),
+                  ('![5]', ['4'], True),
+                  ('![5]', ['5', '4'], False),
+                  ('![5]', ['4', '5'], True)]
+    _run_cases(test_cases)
 
 
 def test_path_checker_future():
     formula = 'F([5])'
-    pc = PathChecker(formula, ['5'])
-    assert pc.truth == True
-    pc = PathChecker(formula, ['4'])
-    assert pc.truth == False
-    pc = PathChecker(formula, ['5', '4'])
-    assert pc.truth == True
-    pc = PathChecker(formula, ['4', '5'])
-    assert pc.truth == True
+    test_cases = [(formula, ['5'], True),
+                  (formula, ['4'], False),
+                  (formula, ['5', '4'], True),
+                  (formula, ['4', '5'], True)]
+    _run_cases(test_cases)
 
 
 def test_path_checker_global():
     formula = 'G([5])'
-    pc = PathChecker(formula, ['5'])
-    assert pc.truth == True
-    pc = PathChecker(formula, ['4'])
-    assert pc.truth == False
-    pc = PathChecker(formula, ['5', '4'])
-    assert pc.truth == False
-    pc = PathChecker(formula, ['5', '5'])
-    assert pc.truth == True
+    test_cases = [(formula, ['5'], True),
+                  (formula, ['4'], False),
+                  (formula, ['5', '4'], False),
+                  (formula, ['5', '5'], True)]
+    _run_cases(test_cases)
