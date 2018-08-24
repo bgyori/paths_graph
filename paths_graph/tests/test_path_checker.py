7aa82d95ebb3531efc0b80b58cfb0c4a81e68698
Ben Gyori <ben.gyori@gmail.com>
2018-08-24 18:37:07 -0400

    Add tests for future and global

--- paths_graph/tests/test_path_checker.py
+++ paths_graph/tests/test_path_checker.py
@@ -11,3 +11,27 @@ def test_path_checker_batch_atomic():
     assert pc.truth == True
     pc = PathChecker(formula, ['4', '5'])
     assert pc.truth == False
+
+
+def test_path_checker_future():
+    formula = 'F([5])'
+    pc = PathChecker(formula, ['5'])
+    assert pc.truth == True
+    pc = PathChecker(formula, ['4'])
+    assert pc.truth == False
+    pc = PathChecker(formula, ['5', '4'])
+    assert pc.truth == True
+    pc = PathChecker(formula, ['4', '5'])
+    assert pc.truth == True
+
+
+def test_path_checker_global():
+    formula = 'G([5])'
+    pc = PathChecker(formula, ['5'])
+    assert pc.truth == True
+    pc = PathChecker(formula, ['4'])
+    assert pc.truth == False
+    pc = PathChecker(formula, ['5', '4'])
+    assert pc.truth == False
+    pc = PathChecker(formula, ['5', '5'])
+    assert pc.truth == True
